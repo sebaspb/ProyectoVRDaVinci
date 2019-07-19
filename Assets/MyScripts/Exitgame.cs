8d42399b0e9de27cab2797a0d3e6b8340a0e18a2
sebaspb0202 <Tecnoparque Sebas>
2019-07-19 11:22:25 -0500

    Se arreglan los tamaños y la rotación de los textos para que sea cómodo leer las descripciones

---
+++ Assets/MyScripts/Exitgame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exitgame : MonoBehaviour
+{
+    
+    public void Salir()
+    {
+
+        Application.Quit();
+
+    }
+}
